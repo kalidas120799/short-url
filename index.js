2446e5579f5572ac6b25ee148682e8b28e894b56
Kalidas <s.kalidas120799@gmail.com>
2021-08-11 09:34:20 +0530

    port update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const morgan = require("morgan")
 const dotenv = require("dotenv").config()
-const port = 3001 || process.env.PORT
+const port =process.env.PORT || 3001
 var app = express()
 
 //database
